b7617b1c8fae2058cb4e35333c65cb37eab47c9c
Magnus Kokk <magnus@kokk.eu>
2019-07-25 11:42:05 +0300

    Formatting

--- app/api/echo/api.go
+++ app/api/echo/api.go
@@ -13,7 +13,6 @@ type API struct {
 
 // Echo returns the same request as a reply.
 func (api *API) Echo(ctx context.Context, req *echov1.EchoRequest) (*echov1.EchoResponse, error) {
-
 	return &echov1.EchoResponse{
 		Message: req.GetMessage(),
 	}, nil
